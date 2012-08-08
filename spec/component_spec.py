f6be2a6c09f88308e8e3e79e4f3eb14bab1e2f45
andy c <t2ahc@sad-panda.local>
2012-08-08 14:26:18 -0500

    build component and composer classes

--- spec/component_spec.py
+++ spec/component_spec.py
@@ -8,9 +8,68 @@ q = os.path.split(os.path.dirname(p))
 sys.path.append(os.path.join(q[0], "src"))
 del p, q # keep globals clean
 
+import component as c
 import unittest
+import random
+from math import pi,e
 
-# Now write some specs...
+class ComponentSpec(unittest.TestCase):
+    def setUp(self):
+        self.test_amp = random.random() * 10.0
+        self.test_decay = 2.0 * (random.random() - 0.5)
+        self.test_freq = random.uniform(0.0001, 0.02)
+        self.test_phase = (random.random() - 0.5) * 2.0 * pi
+        self.test_offset = random.random() * (1.0 / self.test_freq)
+        self.pcase = c.Component(self.test_amp,
+                                 self.test_decay,
+                                 self.test_freq,
+                                 phase=self.test_phase)
+        self.ocase = c.Component(self.test_amp,
+                                 self.test_decay,
+                                 self.test_freq,
+                                 offset=self.test_offset)
+    
+    def test_initialization(self):
+        for case in [self.pcase, self.ocase]:
+            self.assertEqual(self.test_amp, case.amp)
+            self.assertEqual(self.test_decay, case.decay)
+            self.assertEqual(self.test_freq, case.freq)
+            self.assertEqual(1.0/self.test_freq, case.period)
+        self.assertEqual(self.test_phase, self.pcase.phase)
+        self.assertEqual(self.test_offset, self.ocase.offset)
+    
+    def test_conversion_to_offset(self):
+        case = self.pcase
+        self.assertTrue(case.offset >= 0)
+        self.assertTrue(case.offset <= (1.0/self.test_freq))
+    
+    def test_conversion_to_phase(self):
+        case = self.ocase
+        self.assertTrue(case.phase >= -1*pi)
+        self.assertTrue(case.phase <= pi)
+    
+    def test_o_tuple(self):
+        t = self.ocase.to_tuple()
+        self.assertEqual(self.test_amp, t[0])
+        self.assertEqual(self.test_decay, t[1])
+        self.assertEqual(self.test_freq, t[2])
+        self.assertEqual(self.test_offset, t[3])
+    
+    def test_p_tuple(self):
+        t = self.pcase.to_tuple(True)
+        self.assertEqual(self.test_amp, t[0])
+        self.assertEqual(self.test_decay, t[1])
+        self.assertEqual(self.test_freq, t[2])
+        self.assertEqual(self.test_phase, t[3])
+    
+    def test_value(self):
+        for t in range(100):
+            m = e ** (-1 * self.test_decay*t)
+            self.assertTrue(abs(self.pcase.value(t)) <= m*self.test_amp)
+            self.assertTrue(abs(self.ocase.value(t)) <= m*self.test_amp)
+    
+    def tearDown(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
--- spec/recomposer_spec.py
+++ spec/recomposer_spec.py
@@ -9,8 +9,53 @@ sys.path.append(os.path.join(q[0], "src"))
 del p, q # keep globals clean
 
 import unittest
+import component as c
+import recomposer as r
+import random
 
-# Now write some specs...
+class MultipleSpec(unittest.TestCase):
+    def setUp(self):
+        self.test_amp = 10.0
+        self.test_comps = [
+            c.Component(1.0, 0.0, 1.0/40.0),
+            c.Component(2.0, 0.0, 1.0/20.0),
+            c.Component(3.0, 0.0, 1.0/10.0),
+            c.Component(4.0, 0.0, 1.0/5.0),
+            ]
+        
+        self.test_bias = (random.random() - 0.5) * 20.0
+        self.case = r.Recomposer(self.test_comps, self.test_bias)
+    
+    def test_time_series(self):
+        n = random.randint(10,1000)
+        series = self.case.time_series(n)
+        
+        self.assertTrue(len(series) == n)
+        
+        for v in series:
+            self.assertTrue(abs(v - self.test_bias) <= self.test_amp)
+    
+    def tearDown(self):
+        pass
+
+class SingletonSpec(unittest.TestCase):
+    def setUp(self):
+        self.test_amp = 15.0
+        self.test_comp = c.Component(self.test_amp, 0.0, 0.10, offset=0.0)
+        self.test_bias = (random.random() - 0.5) * 20.0
+        self.case = r.Recomposer([self.test_comp], self.test_bias)
+    
+    def test_time_series(self):
+        n = random.randint(10,1000)
+        series = self.case.time_series(n)
+        
+        self.assertTrue(len(series) == n)
+        
+        for v in series:
+            self.assertTrue(abs(v - self.test_bias) <= self.test_amp)
+    
+    def tearDown(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
