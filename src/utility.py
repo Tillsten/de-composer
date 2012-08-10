dbeabc6fe7854fc72437b5367736388f42bedfbb
andy c <t2ahc@sad-panda.local>
2012-08-10 08:03:50 -0500

    stub out utilities

---
+++ src/utility.py
@@ -0,0 +1,10 @@
+#!/usr/bin/epython
+
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# source goes here.
+
+if __name__ == "__main__":
+    print "This module provides generic utilities."
