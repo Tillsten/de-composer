620dbdafe15a319343fd96988b21a528e37d8331
andy c <t2ahc@sad-panda.local>
2012-08-10 08:17:45 -0500

    build utility module

--- spec/utility_spec.py
+++ spec/utility_spec.py
@@ -9,8 +9,50 @@ sys.path.append(os.path.join(q[0], "src"))
 del p, q # keep globals clean
 
 import unittest
+import random
+import utility as u
 
-# now write some meaningful specs...
+class SpecMakerSpec(unittest.TestCase):
+    
+    def test_zeroth_case(self):
+        made = u.make_specs(0)
+        self.assertEqual([], made)
+    
+    def test_random_case(self):
+        n = random.randint(1,20)
+        made = u.make_specs(n)
+        self.assertEqual(n, len(made))
+        for spec in made:
+            self.assertTrue(isinstance(spec, tuple))
+            self.assertEqual(len(spec), 4)
+
+class MeanSqErrorSpec(unittest.TestCase):
+    
+    def test_zero_error(self):
+        a = range(10)
+        b = a[:]
+        err = u.mean_sq_error(a,b)
+        self.assertEqual(0, err)
+    
+    def test_unequal_lengths(self):
+        a = range(10)
+        b = range(100)
+        err = u.mean_sq_error(a,b)
+        self.assertEqual(0, err)
+    
+    def test_errors(self):
+        a = [1,1,1]
+        b = [1,2,3]
+        err = u.mean_sq_error(a,b)
+        expected = 5.0/3.0
+        self.assertEqual(err, expected)
+
+class DeltasSpec(unittest.TestCase):
+    def test_deltas(self):
+        a = [1,1,1]
+        b = [2,-2,2]
+        ds = u.deltas(a,b)
+        self.assertEqual(ds, [1,-3,1])
 
 if __name__ == "__main__":
     unittest.main()
--- src/utility.py
+++ src/utility.py
@@ -3,8 +3,28 @@
 import sys
 import os.path
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+import random
 
-# source goes here.
+def make_specs(ncomps):
+    """Utility for randomly generating Periodic [signaller] specs."""
+    amps = [random.random() * 10.0 for i in range(ncomps)]
+    damps = [random.random() / 60.0 for i in range(ncomps)]
+    freqs = [1.0 / random.randrange(20.0, 200.0) for i in range(ncomps)]
+    offsets = [random.randint(0,50) for i in range(ncomps)]
+    
+    specs = zip(amps, damps, freqs, offsets)
+    return specs
+
+def mean_sq_error(lista, listb):
+    """Easy calculation for mean squared error between two series."""
+    pairs = zip(lista, listb)
+    sq_errors = [(a - b)**2 for (a,b) in pairs]
+    return sum(sq_errors) / float(len(pairs))
+
+def deltas(target, actual):
+    """Provides a list of signed deltas between target and actual series."""
+    pairs = zip(target, actual)
+    return [(a-t) for (t,a) in pairs]
 
 if __name__ == "__main__":
     print "This module provides generic utilities."
